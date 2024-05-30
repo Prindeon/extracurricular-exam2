bfd784002b3f451b05bec2ce03750cceeb92594e
Prindeon <marius.k@hotmail.dk>
2024-05-30 15:13:49 +0200

    smoothloading

--- app.js
+++ app.js
@@ -2,19 +2,32 @@
 
 window.addEventListener("load", initApp); // When the page is loaded, run initApp function
 
+
+// Makes the individual project cards load smoothly one after the other.
+function smoothLoad() {
+  document.querySelectorAll(".project-card").forEach((card, index) => {
+    setTimeout(() => {
+      card.classList.add('visible')
+    }, index * 300)
+  })
+}
+
 // Function to initialize the Web App
 async function initApp() {
   const projects = await getProjects()
   console.log(projects)
   showProjectsContainer(projects)
+  smoothLoad()
 }
 
+// Gets the json data from the wordpress cms
 async function getProjects() {
   const response = await fetch("https://shop.mjkieldsen.com/wp-json/wp/v2/projects?acf_format=standard")
   const data = await response.json()
   return data
 }
 
+// displays the actual data in the html
 function showProjectsContainer(projects) {
   const projectsContainer = document.querySelector("#projects-container")
 
@@ -26,7 +39,7 @@ function showProjectsContainer(projects) {
       /*html*/
       `
       <article class="project-card">
-        <img src="${project.acf.image}"/>
+        <img src="${project.acf.image}" alt="${project.title.rendered}"/>
         <p class="client">Client: <i>${project.acf.client}</i></p>
         <h2>${project.title.rendered}</h2>
         <p>${project.acf.description}</p>
